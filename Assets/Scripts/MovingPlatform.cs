22b51f857b54da4b33ad03b24b5acbf6db67f151
adamkahil <adam.kahil@ontariotechu.net>
2020-10-06 17:54:07 -0400

    BaseProject

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿// class for the player
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float movementSpeed = 10.0F;
+
+    protected float speedMult = 1.0F;
+    protected float knockbackMult = 1.0F;
+    protected float jumpMult = 1.0F;
+    protected float sturdyMult = 1.0F;
+
+    // TODO: add files for model?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        {
+            // forward and backward movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, 0, movementSpeed * speedMult * Time.deltaTime));
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, 0, -movementSpeed * speedMult * Time.deltaTime));
+            }
+
+            // leftward and rightward movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-movementSpeed * speedMult * Time.deltaTime, 0, 0));
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(movementSpeed * speedMult * Time.deltaTime, 0, 0));
+            }
+
+
+        }
+    }
+}
