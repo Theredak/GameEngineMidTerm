265f7fa43171aa10d910af5ca178ae7f2558df93
adamkahil <adam.kahil@ontariotechu.net>
2020-10-08 19:30:11 -0400

    Finished Progress Bar

---
+++ Assets/Scripts/Checkpoint3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Checkpoint3 : MonoBehaviour
+{
+    [SerializeField] public Transform Player;
+    [SerializeField] public Transform respawnPoint;
+
+    public ProgressBar _progressbar;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = respawnPoint.transform.position;
+        _progressbar.IncrementProgress(0.2f);
+
+    }
+}
---
+++ Assets/Scripts/Wall2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall2 : MonoBehaviour
+{
+    Vector3 pointA = new Vector3(433, 56, -390);
+    Vector3 pointB = new Vector3(448, 56, -390);
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+    }
+}
